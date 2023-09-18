757edb95d5542e4de108011744bbe5ebd1facb21
aychid <aydinnajlhossaini@hotmail.com>
2023-09-18 16:42:07 +0200

    Uploaded skeleton files for calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+add = function(num1, num2){
+    return num1 + num2;
+}
+
+subtract = function(num1,num2){
+    return num1 + num2
+}
+
+multiply = function(num1,num2){
+    return num1 * num2
+}
+
+divide = function(num1,num2){
+    return num1 / num2
+}
+
+operate = function(operator, num1, num2){
+    return multiply(num1,num2)
+}
