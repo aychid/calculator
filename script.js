d4434c25c20d6c858f1fd7a4db997ddd8aaab086
aychid <aydinnajlhossaini@hotmail.com>
2023-09-19 11:11:55 +0200

    Added operator function and operator variable
    Makes calculator functions easier to call

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ divide = function(num1,num2){
     return num1 / num2
 }
 
-operate = function(operator, num1, num2){
-    return multiply(num1,num2)
+let number1;
+let number2;
+let operator;
+
+operate = function(num1, operator, num2){
+    return operator(num1,num2)
 }
+
