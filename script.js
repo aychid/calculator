07d46f68f38e6adea3c0142edac7694825e02ee0
aychid <aydinnajlhossaini@hotmail.com>
2024-03-22 02:47:30 +0100

    Refactor whole calculator including html, css and js

--- script.js
+++ script.js
@@ -14,11 +14,63 @@ divide = function(num1,num2){
     return num1 / num2
 }
 
-let number1;
-let number2;
-let operator;
+var number1;
+var number2;
+var operator;
 
 operate = function(num1, operator, num2){
-    return operator(num1,num2)
+    if (operator == division && num2 ==0 ) {
+        alert("Null division")
+    }
+    else {
+        return operator(num1,num2)
+    }
 }
 
+const screen = document.querySelector(".screen")
+const numButtons = document.querySelectorAll(".number")
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (number1 === undefined ) {
+            number1 = parseFloat(`${button.innerHTML}`);
+            screen.innerHTML = number1;
+        }
+        else {
+            number2 = parseFloat(`${button.innerHTML}`);
+            screen.innerHTML = number2;            
+        }
+    })
+})
+
+const equalButton = document.querySelector(".equal")
+equalButton.addEventListener('click', () => {
+    let result = operate(number1, operator, number2)
+    screen.innerHTML = result
+})
+
+const addition = document.getElementById("addition")
+addition.addEventListener('click', () => {
+    operator = add
+})
+
+const subtraction = document.getElementById("subtraction")
+subtraction.addEventListener('click', () => {
+    operator = subtract
+})
+
+const multiplication = document.getElementById("multiplication")
+multiplication.addEventListener('click', () => {
+    operator = multiply
+})
+
+const division = document.getElementById("division")
+division.addEventListener('click', () => {
+    operator = divide
+})
+
+const clearButton = document.getElementById("clear")
+clearButton.addEventListener('click', () => {
+    screen.innerHTML = 0;
+    number1 = undefined;
+})
