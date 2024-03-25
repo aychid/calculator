a75703dcda9689f2011b3f50cfb488555b940d0b
aychid <aydinnajlhossaini@hotmail.com>
2024-03-24 23:36:23 -0400

    refactor: refactor with AI, clean up code structure and remove redundant operator functions

--- script.js
+++ script.js
@@ -1,109 +1,153 @@
-add = function(num1, num2){
-    return num1 + num2;
+const operators = {
+    "+": (num1, num2) => num1 + num2,
+    "-": (num1, num2) => num1 - num2,
+    "x": (num1, num2) => num1 * num2,
+    "÷": (num1, num2) => num2 === 0 ? "Error" : num1 / num2
+};
+
+let display = '';
+let number1 = '';
+let number2 = '';
+let currentOperator = '';
+
+const screen = document.querySelector(".screen");
+
+function updateScreen() {
+    screen.innerHTML = display || '0';
 }
 
-subtract = function(num1,num2){
-    return num1 - num2
+function clear() {
+    display = '';
+    number1 = '';
+    number2 = '';
+    currentOperator = '';
+    updateScreen();
 }
 
-multiply = function(num1,num2){
-    return num1 * num2
-}
-
-divide = function(num1,num2){
-    return num1 / num2
-}
-
-var number1;
-var number2;
-var operator;
-let display;
-
-operate = function(num1, operator, num2){
-    if (operator == division && num2 == 0 ) {
-        alert("Null division")
+function operate(){
+    if ( currentOperator === '÷' && number2 === '0' ) {
+        // screen.textContent = "Error";
+        // return;
+        display = "Null division";
+        // clear();
     }
     else {
-        return operator(num1,num2)
-        
+        display = operators[currentOperator](parseFloat(number1), parseFloat(number2)).toString();
     }
+
+    number1 = display;
+    number2 = '';
+    currentOperator = '';
+    updateScreen();
 }
 
-const screen = document.querySelector(".screen")
-const numButtons = document.querySelectorAll(".number")
-var isNum1 = true;
+document.querySelectorAll(".number, .operator").forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.textContent;
+        if (Number.isInteger(parseInt(value))) {
+            display += value;
+            if (currentOperator === '') {
+                number1 += value;
+            } else {
+                number2 += value;
+            }
+        } else if (value === 'C' ) {
+            clear();
+        } else if (value === '=') {
+            operate();
+        } else {
+            currentOperator = value;
+            display = '';
+        }
+        updateScreen();
+    });
+});
 
-numButtons.forEach(button => {
-    button.addEventListener('click', () => { // TODO fix numbers not resetting properly and refactor to be more clean
+// operate = function(num1, operator, num2){
+//     if (operator == division && num2 == 0 ) {
+//         alert("Null division")
+//     }
+//     else {
+//         return operator(num1,num2)
         
-        if (display === undefined && isNum1 === true) {
-            display = `${button.innerHTML}`;   
-        }
-        else if (display != undefined && isNum1 === true) {
-            display = display + `${button.innerHTML}`;
-        }
-        else if (display === undefined && isNum1 === false ){
-            display = `${button.innerHTML}`;   
-        } 
-        else if (display != undefined && isNum1 == false) {
-            display = display + `${button.innerHTML}`;
-        } 
-        else {
-            console.log("Error2")
-        }
+//     }
+// }
 
-        screen.innerHTML = display
 
-        if (operator === undefined) {
-            number1 = parseFloat(display);
-        }
-        else {
-            number2 = parseFloat(display);
-        }
-    })
-})
+// const numButtons = document.querySelectorAll(".number")
+// var isNum1 = true;
 
-const equalButton = document.querySelector(".equal")
-equalButton.addEventListener('click', () => {
-    let result = operate(number1, operator, number2);
-    number1 = result;
-    screen.innerHTML = result;
-})
+// numButtons.forEach(button => {
+//     button.addEventListener('click', () => { // TODO fix numbers not resetting properly and refactor to be more clean
+        
+//         if (display === undefined && isNum1 === true) {
+//             display = `${button.innerHTML}`;   
+//         }
+//         else if (display != undefined && isNum1 === true) {
+//             display = display + `${button.innerHTML}`;
+//         }
+//         else if (display === undefined && isNum1 === false ){
+//             display = `${button.innerHTML}`;   
+//         } 
+//         else if (display != undefined && isNum1 == false) {
+//             display = display + `${button.innerHTML}`;
+//         } 
+//         else {
+//             alert("Error 404")
+//         }
 
-const addition = document.getElementById("addition")
-addition.addEventListener('click', () => {
-    operator = add;
-    isNum1 = false;
-    display = undefined;
-})
+//         screen.innerHTML = display
 
-const subtraction = document.getElementById("subtraction")
-subtraction.addEventListener('click', () => {
-    operator = subtract;
-    isNum1 = false;
-    display = undefined;
-})
+//         if (operator === undefined) {
+//             number1 = parseFloat(display);
+//         }
+//         else {
+//             number2 = parseFloat(display);
+//         }
+//     })
+// })
 
-const multiplication = document.getElementById("multiplication")
-multiplication.addEventListener('click', () => {
-    operator = multiply;
-    isNum1 = false;
-    display = undefined;
-})
+// const equalButton = document.querySelector(".equal")
+// equalButton.addEventListener('click', () => {
+//     let result = operate(number1, operator, number2);
+//     number1 = result;
+//     screen.innerHTML = result;
+// })
 
-const division = document.getElementById("division")
-division.addEventListener('click', () => {
-    operator = divide;
-    isNum1 = false;
-    display = undefined;
-})
+// const addition = document.getElementById("addition")
+// addition.addEventListener('click', () => {
+//     operator = add;
+//     isNum1 = false;
+//     display = undefined;
+// })
 
-const clearButton = document.getElementById("clear")
-clearButton.addEventListener('click', () => {
-    screen.innerHTML = 0;
-    display = undefined;
-    number1 = undefined;
-    number2 = undefined
-    operator = undefined;
-    isNum1 = true;
-})
+// const subtraction = document.getElementById("subtraction")
+// subtraction.addEventListener('click', () => {
+//     operator = subtract;
+//     isNum1 = false;
+//     display = undefined;
+// })
+
+// const multiplication = document.getElementById("multiplication")
+// multiplication.addEventListener('click', () => {
+//     operator = multiply;
+//     isNum1 = false;
+//     display = undefined;
+// })
+
+// const division = document.getElementById("division")
+// division.addEventListener('click', () => {
+//     operator = divide;
+//     isNum1 = false;
+//     display = undefined;
+// })
+
+// const clearButton = document.getElementById("clear")
+// clearButton.addEventListener('click', () => {
+//     screen.innerHTML = 0;
+//     display = undefined;
+//     number1 = undefined;
+//     number2 = undefined
+//     operator = undefined;
+//     isNum1 = true;
+// })
